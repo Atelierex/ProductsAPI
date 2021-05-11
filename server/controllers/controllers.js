2e807c9669a65aeb3ecafd5d30f5f7548b474ad8
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-05-11 11:06:57 -0700

    Add handling for query to /products/:product_id

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -3,14 +3,29 @@ const con = require('../../mySQL/mySQL.js');
 const productsHandler = (req, res) => {
   let count = req.query.count === undefined ? 5 : req.query.count;
   let queryString = `SELECT * FROM product LIMIT ${count}`;
-  con.query(queryString, (err, rows) => {
+  con.query(queryString, (err, productsData) => {
     if (err) {
       res.status(500);
       res.end();
     } else {
-      res.send(rows);
+      res.send(productsData);
+    }
+  });
+};
+
+const productHandler = (req, res) => {
+  let queryString = `
+  SELECT JSON_OBJECT('id', product.id,'name', product.name,'slogan', product.slogan,'description', product.description,'category', product.category, 'default_price', product.default_price, 'features', (select json_arrayagg(json_object('feature', features.feature,'value', features.value)) FROM features WHERE features.product_id = product.id) ) FROM product AS product WHERE id=${req.params.product_id}`;
+  con.query(queryString, (err, productData) => {
+    if (err) {
+      res.status(500);
+      res.end();
+    } else {
+      let key = Object.keys(productData[0])[0];
+      res.send(productData[0][key]);
     }
   });
 };
 
 exports.productsHandler = productsHandler;
+exports.productHandler = productHandler;
--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@ const app = express();
 const port = 4401;
 const controllers = require('./controllers/controllers.js');
 
+
 app.get('/products', controllers.productsHandler);
+app.get('/products/:product_id', controllers.productHandler);
+
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
