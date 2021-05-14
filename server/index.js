da35056edd81a43974aad25fb5849d21f468dddd
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-05-13 21:55:26 -0700

    Add Pathway for Public Folder

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 4401;
 const controllers = require('./controllers/controllers.js');
 
+app.use(express.static('public'))
 
 app.get('/products', controllers.productsHandler);
 app.get('/products/:product_id', controllers.productHandler);
